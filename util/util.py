9d78430328e2a6b67ae74c41a8b3fe9e86dba081
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2014-11-09 15:04:04 -0800

    Stdout and stderr to file as well

--- util/util.py
+++ util/util.py
@@ -12,6 +12,49 @@ import pathSetter;
 import datetime;
 import smtplib;
 
+class TeeStdOut(object):
+    def __init__(self, name, mode='w'):
+        self.file = open(name, mode);
+        self.stdout = sys.stdout;
+        sys.stdout = self;
+    def close(self):
+        if self.stdout is not None:
+            sys.stdout = self.stdout
+            self.stdout = None
+        if self.file is not None:
+            self.file.close()
+            self.file = None
+    def write(self, data):
+        self.file.write(data)
+        self.stdout.write(data)
+    def flush(self):
+        self.file.flush()
+        self.stdout.flush()
+    def __del__(self):
+        self.close()
+
+class TeeStdErr(object):
+    def __init__(self, name, mode='w'):
+        self.file = open(name, mode);
+        self.stderr = sys.stderr;
+        sys.stderr = self;
+    def close(self):
+        if self.stderr is not None:
+            sys.stderr = self.stderr
+            self.stderr = None
+        if self.file is not None:
+            self.file.close()
+            self.file = None
+    def write(self, data):
+        self.file.write(data)
+        self.stderr.write(data)
+    def flush(self):
+        self.file.flush()
+        self.stderr.flush()
+    def __del__(self):
+        self.close()
+
+
 def executeAsSystemCall(commandToExecute):
 	print "Executing: "+commandToExecute;
 	if (os.system(commandToExecute)):
