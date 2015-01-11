e784f2bb10c04cbcc76c852d80a54ae013b59cbe
AvantiShri <avanti.shrikumar@gmail.com>
2015-01-11 00:10:48 -0800

    Recentering code

--- bedtoolsExtension/recenterSequences.py
+++ bedtoolsExtension/recenterSequences.py
@@ -2,16 +2,15 @@
 import os;
 scriptsDir = os.environ.get("UTIL_SCRIPTS_DIR");
 if (scriptsDir is None):
-	raise Exception("Please set environment variable UTIL_SCRIPTS_DIR");
+    raise Exception("Please set environment variable UTIL_SCRIPTS_DIR");
 import sys;
 sys.path.insert(0,scriptsDir);
 import pathSetter;
 import argparse;
 import fileProcessing as fp;
 import util;
-import transformSequencesIntoNeuralNetworkInput_function as tsinnif; 
 
-def getRegionOfLengthFromCenterOf(start, end, length):
+def getRegionCenteredAround(start, end, length):
     range = getRange(end-start, length);
     return (start + range[0], start + range[1]);
 
@@ -21,51 +20,48 @@ def getRange(lengthOfInput, lengthOfOutput):
     midpoint = int(lengthOfInput/2);
     return (midpoint-halfLength, midpoint+remainingLength); 
 
-def outputFileFromInputFile(options):
-	return fp.getFileNameParts(options.inputFile).getFilePathWithTransformation(transformation=lambda x: 'sequencesCentered-'+str(options.sequencesLength)+'_'+x);
+def outputFileFromInputFile(inputFile):
+    return fp.getFileNameParts(inputFile).getFilePathWithTransformation(transformation=lambda x: 'sequencesCentered-'+str(options.sequencesLength)+'_'+x);
 
 def outputTitle(options,sep="\t"):
-	toReturn="id";
-	toReturn+=sep+"cluster";
-	toReturn+=sep+"sequence";
-	toReturn += "\n";
-	return toReturn;
+    toReturn="chrom";
+    toReturn+=sep+"cluster";
+    toReturn+=sep+"sequence";
+    toReturn += "\n";
+    return toReturn;
 
 def recenterSequences(options):
-	options.outputFile = options.outputFile if (options.outputFile is not None) else outputFileFromInputFile(options);
-	
-	def transformation(content):
-		toReturn = content[options.columnIdIndex];
-		toReturn += "\t"+content[options.clusterColumnIndex];
-		toReturn += "\t"+tsinnif.centerSequence(
-				content[options.sequencesColumnIndex]
-				,options.sequencesLength);	
-		toReturn += "\n";
-		return toReturn;
+    outputFileHandle = fp.getFileHandle(options.outputFile, 'w');
+    outputFileHandle.close();
+    for inputFile in options.inputFiles:
+        options.outputFile = options.outputFile if (options.outputFile is not None) else outputFileFromInputFile(inputFile);
+        outputFileHandle = fp.getFileHandle(options.outputFile, 'a');
+        def action(inp, lineNumber):
+            arrToPrint = [];
+            arrToPrint.extend([inp[x] for x in options.auxillaryColumnsBefore]);
+            (startBase, endBase) = getRegionCenteredAround(int(inp[options.startColIndex]), int(inp[options.endColIndex]), options.sequencesLength);
+            arrToPrint.extend([str(startBase), str(endBase)]);
+            arrToPrint.extend([inp[x] for x in options.auxillaryColumnsAfter]);
+            outputFileHandle.write(("\t".join(arrToPrint))+"\n");
+        
+        fp.performActionOnEachLineOfFile(
+            fp.getFileHandle(inputFile)
+            , transformation = util.chainFunctions(fp.trimNewline, fp.splitByTabs)
+            , action = action
+            , ignoreInputTitle = False
+        );
+        
 
-	fp.transformFile(
-		fp.getFileHandle(options.inputFile)
-		,options.outputFile
-		,preprocessing=util.chainFunctions(
-			fp.trimNewline
-			,fp.splitByTabs
-		) 
-		,filterFunction=filterFunction
-		,transformation=transformation
-		,progressUpdates=options.progressUpdates
-		,outputTitleFromInputTitle = lambda x: outputTitle(options)
-		,ignoreInputTitle=True
-	)
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser();
-	parser.add_argument('inputFile');
-	parser.add_argument('--outputFile', help="if not supplied, output will be named as input file with 'sequencesCentered-size_' prefixed");
-	parser.add_argument('--progressUpdates', type=int, default=10000);
-	parser.add_argument('--sequencesLength', default=150, help="Region of this size centered on the center will be extracted");
-	parser.add_argument('--auxillaryColumnsBefore', default=[0]);
+    parser = argparse.ArgumentParser();
+    parser.add_argument('inputFiles', nargs='+');
+    parser.add_argument('--outputFile', help="if not supplied, output will be named as input file with 'sequencesCentered-size_' prefixed");
+    parser.add_argument('--progressUpdates', type=int, default=10000);
+    parser.add_argument('--sequencesLength', default=150, type=int, help="Region of this size centered on the center will be extracted");
+    parser.add_argument('--auxillaryColumnsBefore', default=[0]);
     parser.add_argument('--auxillaryColumnsAfter', default=[]);
-	parser.add_argument('--startColIndex',default=1);
-	parser.add_argument('--endColIndex', default=2);
-	args = parser.parse_args();	
-	recenterSequences(args);
+    parser.add_argument('--startColIndex',default=1);
+    parser.add_argument('--endColIndex', default=2);
+    args = parser.parse_args();    
+    recenterSequences(args);
