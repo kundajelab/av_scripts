a8f9d5d58e0e49acca139a80b7ff53c2771d9a75
Avanti Shrikumar <avanti@cherry-gpu1.stanford.edu>
2015-01-15 14:09:44 -0800

    updated emailWhenDone script

--- util/emailWhenDone.py
+++ util/emailWhenDone.py
@@ -14,14 +14,16 @@ def writeFileAndEmailWhenDone(args):
     fileHandle = fp.getFileHandle(args.shPath, 'w');
     fileHandle.write(" ".join(args.args));
     fileHandle.close();
-    os.system("sh "+args.shPath);
+    outFile = args.shPath+".out";
+    os.system("sh "+args.shPath+" >"+outFile+" 2>&1");
     util.sendEmail(args.email, 'jobRunner@stanford.edu', "Done "+args.shPath, "");
     if (args.dontRm == False):
         os.system("rm "+args.shPath);
+        os.system("rm "+outFile);
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate a qsub file");
-    parser.add_argument('--shPath', help="The path of the qsub .sh file");
+    parser.add_argument('--shPath', default="temp.sh", help="The path of the .sh file. Defaults to temp.sh");
     parser.add_argument('--email', default='avanti@stanford.edu');
     parser.add_argument('args', nargs=argparse.REMAINDER, help="The command that you are actuall submitting through the qsub file");
     parser.add_argument('--dontRm', action='store_true', help='Wont remove file when done');
