1f8377efe6f72d800788c16b4e87ad294092dae8
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2015-06-06 20:07:06 -0700

    condition check periodic commit

--- util/conditionCheck.py
+++ util/conditionCheck.py
@@ -9,14 +9,14 @@ if (scriptsDir is None):
 sys.path.insert(0,scriptsDir);
 
 def getIndentation(indentation):
-    "".join("\t" for i in xrange(indentation));
+    return "".join("\t" for i in xrange(indentation));
 
 class Condition(object):
     def __init__(self):
         self.isSatisfied = None;
     def getDescription(self, indentation):
         raise NotImplementedError();
-    def isSatisfied(self):
+    def getIsSatisfied(self):
         if (self.isSatisfied == None):
             self.isSatisfied = self.computeIsSatisfied();
             assert self.isSatisfied is not None;
@@ -24,18 +24,19 @@ class Condition(object):
     def computeIsSatisfied(self):
         raise NotImplementedError();
     def getSatString(self):
-        if (self.isSatisfied):
+        if (self.getIsSatisfied()):
             return "(Satisfied)";
-        elif (self.isSatisfied==False):
+        elif (self.getIsSatisfied()==False):
             return "(Not Satisfied)";
         else:
-            raise RuntimeError("Satisfied should be set");
+            raise RuntimeError();
 
 class ValueAmongOptions(Condition):
     def __init__(self, val, valName, supportedOptions):
         self.supportedOptions = supportedOptions;
         self.val = val;
         self.valName = valName;
+        super(ValueAmongOptions,self).__init__(); 
     def getDescription(self, indentation):
         return getIndentation(indentation)+"{"+self.getSatString()+" value "+self.valName+" is "+str(self.val)+"; should be in "+("\t".join(str(x) for x in self.supportedOptions))+"\n}"; 
     def computeIsSatisfied(self):
@@ -44,7 +45,8 @@ class ValueAmongOptions(Condition):
 class ValueIsSetInOptions(Condition):
     def __init__(self, options, valName):
         self.options = options;
-        self.valName = valName; 
+        self.valName = valName;
+        super(ValueIsSetInOptions,self).__init__(); 
     def getDescription(self, indentation):
         return getIndentation(indentation)+"{"+self.getSatString()+" value "+self.valName+" must be set}";
     def computeIsSatisfied(self):
@@ -56,6 +58,7 @@ class ValueIsSetInOptions(Condition):
 class Notter(Condition):
     def __init__(self, condition):
         self.condition = condition;
+        super(Notter,self).__init__(); 
     def getDescription(self, indentation):
         return getIndentation(indentation)+"{"+self.getSatString()+" Following should by unsatisfied: {\n"+self.condition.getDescription(indentation+1)+"\n}";
     def computeIsSatisfied(self):
@@ -67,11 +70,12 @@ class ArrayCondition(Condition):
             conds: array of conditions
         """
         self.conds = conds;
+        super(ArrayCondition,self).__init__(); 
     def getArrayConditionString(self):
         raise NotImplementedError();
     def getDescription(self, indentation):
         return (getIndentation(indentation)+"{"+self.getSatString()+" "+self.getArrayConditionString()+" [\n"
-                        +"\n".join(x.getDescription(indentation+1))
+                        +"\n".join(x.getDescription(indentation+1) for x in self.conds)
                 +"]}");
     def computeIsSatisfied(self):
         raise NotImplementedError();
@@ -80,13 +84,13 @@ class Or(ArrayCondition):
     def getArrayConditionString(self):
         return "at least one of the following must be true:"
     def computeIsSatisfied(self):
-        return any(x.isSatisfied() for x in self.conds);
+        return any(x.getIsSatisfied() for x in self.conds);
 
 class All(ArrayCondition):
     def getArrayConditionString(self):
         return "all of the following must be true:";
     def computeIsSatisfied(self):
-        return all(x.isSatisfied() for x in self.conds);
+        return all(x.getIsSatisfied() for x in self.conds);
 
 class AtMostOne(ArrayCondition):
     def getArrayConditionString(self):
@@ -98,7 +102,24 @@ class ExactlyOne(ArrayCondition):
     def getArrayConditionString(self):
         return "only one of the following can be true"; 
     def computeIsSatisfied(self):
-        return sum(self.conds) == 1;
+        return sum(x.getIsSatisfied() for x in self.conds) == 1;
+
+if __name__ == "__main__":
+    class DummyClass(object):
+        pass;
+    options = DummyClass();
+    options.val1 = "abcd";
+    check1 = ValueIsSetInOptions(options, "val1");
+    check2 = ValueIsSetInOptions(options, "val2");
+    check3 = Notter(check2);
+    check4 = All([check1, check2]);
+    check5 = ExactlyOne([check1, check3, check2]);
+    print("check1",check1.getIsSatisfied());
+    print("check4",check4.getDescription(0));
+    print("check4",check4.getIsSatisfied());
+    print("check5", check5.getDescription(0));
+    
 
 
+  
 
