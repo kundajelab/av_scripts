b6d509d47612985c85bc16fcde78370ae25da578
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2015-06-11 19:04:25 -0700

    blah

--- synthetic/syntheticDemo.py
+++ synthetic/syntheticDemo.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
+import os;
+import sys;
+scriptsDir = os.environ.get("UTIL_SCRIPTS_DIR");
+if (scriptsDir is None):
+    raise Exception("Please set environment variable UTIL_SCRIPTS_DIR");
+sys.path.insert(0,scriptsDir);
+import pathSetter
 import synthetic;
 
 pathToMotifs = "motifs.txt";
