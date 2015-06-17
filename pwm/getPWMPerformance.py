a721ca9ec1fd5ed282d7e5a8e7f27d6afca795dc
Irene Kaplow <imk1@doublehelix.Stanford.EDU>
2015-06-16 23:42:22 -0700

    I fixed a small bug in getPWMPerformance.py

--- pwm/getPWMPerformance.py
+++ pwm/getPWMPerformance.py
@@ -16,6 +16,7 @@ from itertools import izip;
 import numpy as np;
 from sklearn.cross_validation import train_test_split;
 from sklearn.tree import DecisionTreeClassifier;
+from sklearn.ensemble import RandomForestClassifier;
 from sklearn.grid_search import GridSearchCV;
 from sklearn.metrics import accuracy_score;
 from sklearn.metrics import confusion_matrix;
