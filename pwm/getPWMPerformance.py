e3b8ec99e28ec6996869bf513921bce29ff5a942
Irene Kaplow <imk1@doublehelix.Stanford.EDU>
2015-06-16 23:38:05 -0700

    I added a randomForest option to --classifierType and made it the default.

--- pwm/getPWMPerformance.py
+++ pwm/getPWMPerformance.py
@@ -20,7 +20,7 @@ from sklearn.grid_search import GridSearchCV;
 from sklearn.metrics import accuracy_score;
 from sklearn.metrics import confusion_matrix;
 
-CLASSIFIER_TYPE = util.enum(decisionTree="decisionTree");
+CLASSIFIER_TYPE = util.enum(decisionTree="decisionTree", randomForest="randomForest");
 SCORING = util.enum(roc_auc='roc_auc', accuracy='accuracy', recall='recall');
 
 def runDecisionTree(scoringResultList, scoringResultListTrainValid, scoringResultListTest, labelsTrainValid, labelsTest, options):
@@ -57,11 +57,47 @@ def runDecisionTree(scoringResultList, scoringResultListTrainValid, scoringResul
 	preds = clf.predict(scoringResultList)
 	return [acc, sensitivity, specificity, preds]
 
+def runRandomForest(scoringResultList, scoringResultListTrainValid, scoringResultListTest, labelsTrainValid, labelsTest, options):
+	# Run a decision tree classifier on the top PWMs
+	if (len(labelsTrainValid == 0) == 0) or (len(labelsTrainValid == 1) == 0):
+		# There are no examples in the training/validation set from one of the classes
+		raise RuntimeError("Only one class is present in the training/validation set")
+	ind_0 = labelsTest == 0
+	ind_1 = labelsTest == 1
+	if (len(ind_0) == 0) or (len(ind_1) == 0):
+		# There are no examples in the test set from one of the classes
+		raise RuntimeError("Only one class is present in the test set")
+	tuned_parameters = [{'nestimators': range(1, options.topN + 1), 'max_depth': range(1, options.topN + 1), 'max_features': [options.topN]}]
+	clf = GridSearchCV(RandomForestClassifier(), tuned_parameters, cv=5, n_jobs=4, scoring=str(options.scoring))
+	clf.fit(scoringResultListTrainValid, labelsTrainValid)
+	labelsPred = clf.predict(scoringResultListTest)
+	acc = accuracy_score(labelsTest, labelsPred)
+	sensitivity = accuracy_score(labelsTest[ind_1], labelsPred[ind_1])
+	specificity = accuracy_score(labelsTest[ind_0], labelsPred[ind_0])
+	if options.verbose:
+		# Print information from the classifier
+		print("Best parameters set found:")
+		print(clf.best_params_)
+		print("Grid scores:")
+		for params, meanScore, scores in clf.grid_scores_:
+			# Iterate through the information from the decision tree and print all of it
+			print("%0.3f (+/-%0.03f) for %r" % (meanScore, scores.std() * 2, params))
+		print ("Test accuracy:")
+		print(acc)
+		print ('Test Confusion Matrix:')
+		print(confusion_matrix(labelsTest, labelsPred))
+		print ("Sensitivity: " + str(sensitivity))
+		print ("Specificity: " + str(specificity))
+	preds = clf.predict(scoringResultList)
+	return [acc, sensitivity, specificity, preds]
+
 def runClassifier(scoringResultList, scoringResultListTrainValid, scoringResultListTest, labelsTrainValid, labelsTest, options):
 	# Run the classifier
 	if options.classifierType == CLASSIFIER_TYPE.decisionTree:
 		# Run a decision tree classifier
 		[acc, sensitivity, specificity, preds] = runDecisionTree(scoringResultList, scoringResultListTrainValid, scoringResultListTest, labelsTrainValid, labelsTest, options)
+	elif options.classifierType == CLASSIFIER_TYPE.randomForest:
+		[acc, sensitivity, specificity, preds] = runRandomForest(scoringResultList, scoringResultListTrainValid, scoringResultListTest, labelsTrainValid, labelsTest, options)
 	else:
 		raise RuntimeError("Unsupported classifier "+str(options.classifierType))
 	return [acc, sensitivity, specificity, preds]
@@ -95,7 +131,7 @@ if __name__ == "__main__":
     parser.add_argument("--reverseComplementToo", action="store_true");
     parser.add_argument("--labelsFile", required=True); # This is assumed to have a header; line i corresponds to line i in fileToScore
     parser.add_argument("--outputFile", required=True);
-    parser.add_argument("--classifierType", choices=CLASSIFIER_TYPE.vals, default="decisionTree")
+    parser.add_argument("--classifierType", choices=CLASSIFIER_TYPE.vals, default="randomForest")
     parser.add_argument("--scoring", choices=SCORING.vals, default='roc_auc')
     parser.add_argument("--testFrac", type=float, default=0.3)
     parser.add_argument("--verbose", action="store_true");
