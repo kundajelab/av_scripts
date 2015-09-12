9ac95c53bd1a0177b9772c3965f65270a8a4731e
AvantiShri <avanti.shrikumar@gmail.com>
2015-09-12 14:55:17 -0700

    sendEmail script in

---
+++ exec/sendEmail.py
@@ -0,0 +1,1 @@
+../util/sendEmail.py
--- util/sendEmail.py
+++ util/sendEmail.py
@@ -1,18 +1,28 @@
+#!/usr/bin/env python
 # Import smtplib for the actual sending function
 import smtplib
 
-# Import the email modules we'll need
-from email.mime.text import MIMEText
+def sendEmail(theSubject, theTo, theFrom="av-mail-sender@stanford.edu", theContents=""):
+    from email.mime.text import MIMEText
+    msg = MIMEText(theContents)
 
-msg = MIMEText("Howdy")
+    msg['Subject'] = theSubject
+    msg['From'] = theFrom
+    msg['To'] = ",".join(theTo)
 
-me = 'avanti@stanford.edu';
-you = 'avanti@stanford.edu';
-msg['Subject'] = 'The contents of oink'
-msg['From'] = 'yourmom@stanford.edu'
-msg['To'] = 'avanti@stanford.edu'
+    s = smtplib.SMTP('smtp.stanford.edu')
+    s.starttls();
+    s.sendmail(theFrom, theTo, msg.as_string())
+    s.quit()
 
-s = smtplib.SMTP('smtp.stanford.edu')
-s.starttls();
-s.sendmail(me, [you], msg.as_string())
-s.quit()
+if __name__ == "__main__":
+    import argparse;
+    parser = argparse.ArgumentParser("Script for email sending");
+    parser.add_argument("--to", nargs="+",required=True);
+    parser.add_argument("--sender",default="av-mail-sender@stanford.edu");
+    parser.add_argument("--subject",required=True);
+    parser.add_argument("--contents", default="");
+    options = parser.parse_args();
+    sendEmail(options.subject, options.to, options.sender, options.contents);
+
+
