c19f6aaf2c028e294d990184379bb2896554f00c
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2015-09-26 13:52:29 -0700

    Scripts done

--- adhoc/TFdataFiltering/adhoc_Pou_Nanog_H1ESC/prepNegatives.sh
+++ adhoc/TFdataFiltering/adhoc_Pou_Nanog_H1ESC/prepNegatives.sh
@@ -2,9 +2,11 @@
 #Expects sortRelaxedChipSeqPeaks.sh to have been run
 
 SORTED_RELAXED_CHIPSEQ_PEAKS_NOGZ="sorted_relaxedChipSeqPeaks" #file has gz suffix. sorted by 7th column in descending order.
-SORTED_RELAXED_CHIPSEQ_PEAKS=${SORTED_RELAXED_CHIPSEQ_PEAKS_NOGZ}".gz"
 CELL_TYPE_DNASE_PEAKS="../cellTypeDNasePeaks.gz"
-TOPN=200000 #will only consider this many of the sorted, relaxed chipseq peaks, since they are SUPER relaxed
+TOPN=150000 #will only consider this many of the sorted, relaxed chipseq peaks, since they are SUPER relaxed
+WINDOW=400
+
+SORTED_RELAXED_CHIPSEQ_PEAKS=${SORTED_RELAXED_CHIPSEQ_PEAKS_NOGZ}".gz"
 TOPN_SORTED_PEAKS_FILENAME_NOGZ="top"${TOPN}"_"${SORTED_RELAXED_CHIPSEQ_PEAKS_NOGZ}
 
 zcat $SORTED_RELAXED_CHIPSEQ_PEAKS | head -${TOPN} > $TOPN_SORTED_PEAKS_FILENAME_NOGZ
@@ -12,6 +14,9 @@ gzip $TOPN_SORTED_PEAKS_FILENAME_NOGZ
 TOPN_SORTED_PEAKS_FILENAME=${TOPN_SORTED_PEAKS_FILENAME_NOGZ}".gz"
 
 OUTPUT_FILE="filteredDNase_top"${TOPN}"_relaxedPeaks"
+CENTERED_OUTPUT_FILE="centered_halfWindow"${WINDOW}"_"${OUTPUT_FILE}
 
 bedtools intersect -v -wa -a ${SORTED_RELAXED_CHIPSEQ_PEAKS} -b ${TOPN_SORTED_PEAKS_FILENAME} > $OUTPUT_FILE
 gzip $OUTPUT_FILE
+
+../centerAroundSummit.sh $OUTPUT_FILE".gz" $WINDOW $CENTERED_OUTPUT_FILE
--- adhoc/TFdataFiltering/adhoc_Pou_Nanog_H1ESC/prepPositives.sh
+++ adhoc/TFdataFiltering/adhoc_Pou_Nanog_H1ESC/prepPositives.sh
@@ -1,9 +1,9 @@
+#!/usr/bin/env bash
 #You should run this script in a folder that has the appropriate narrowpeak file linked
 #as a symlink with the name idrChipSeqPeaks
 RAW_POS_FILE_CORE="idrChipSeqPeaks" #assumed to be a gzipped file with .gz suffix
 WINDOW=400 #+/- WINDOW around the summit
-OUTPUT_POS_FILE="centered_halfWindow"${WINDOW}"_"${RAW_POS_FILE_CORE}".bed"
+OUTPUT_POS_FILE="centered_halfWindow"${WINDOW}"_"${RAW_POS_FILE_CORE}
 #The last column represents the offset from the summit
 
-zcat ${RAW_POS_FILE_CORE}".gz" | perl -lape '$chrom=$F[0]; $start=$F[1]; $end=$F[2]; $offset=$F[$#F]; $center=$start+$offset; $halfWindow='${WINDOW}'; $newStart=$center-$halfWindow; $newEnd=$center+$halfWindow; $_="$chrom\t$newStart\t$newEnd"' > $OUTPUT_POS_FILE
-gzip $OUTPUT_POS_FILE
+../centerAroundSummit.sh ${RAW_POS_FILE_CORE}".gz" $WINDOW $OUTPUT_POS_FILE
