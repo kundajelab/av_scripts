bf9a91e85bebb607a680a0381fb38f7d031f32a3
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2015-09-26 12:29:02 -0700

    added comment

--- adhoc/TFdataFiltering/adhoc_Pou_Nanog/prepPositives.sh
+++ adhoc/TFdataFiltering/adhoc_Pou_Nanog/prepPositives.sh
@@ -1,3 +1,5 @@
+#You should run this script in a folder that has the appropriate narrowpeak file linked
+#as a symlink with the name idrChipSeqPeaks
 RAW_POS_FILE_CORE="idrChipSeqPeaks" #assumed to be a gzipped file with .gz suffix
 WINDOW=400 #+/- WINDOW around the summit
 OUTPUT_POS_FILE="centered_halfWindow"${WINDOW}"_"${RAW_POS_FILE_CORE}".bed"
