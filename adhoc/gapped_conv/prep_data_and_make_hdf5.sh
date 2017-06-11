31f0e0330a3708bc245fe0fcbbd46dc1cffc0c63
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2017-06-11 00:54:44 -0700

    it is what it is

--- adhoc/gapped_conv/prep_data_and_make_hdf5.sh
+++ adhoc/gapped_conv/prep_data_and_make_hdf5.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 ##simdata generation - install simdna
+#NOTE THAT VALID AND TRAIN HAVE BEEN SWAPPED; TRAIN SET IS SMALL!
 rm variableSpacingGrammarSimulation_*
-rm motifGrammarSimulation_*
-variableSpacingGrammarSimulation.py --motifName1 GATA_disc1 --motifName2 TAL1_known1 --seqLength 200 --numSeq 1000 --minSpacing 0 --meanSpacing 5 --maxSpacing 10
-motifGrammarSimulation.py --motifName1 GATA_disc1 --motifName2 TAL1_known1 --seqLength 200 --numSeq 1000 --generationSetting twoMotifs
+variableSpacingGrammarSimulation.py --prefix broad --motifName1 GATA_disc1 --motifName2 GATA_disc1 --seqLength 200 --numSeq 1000 --minSpacing 0 --meanSpacing 5 --maxSpacing 10
+variableSpacingGrammarSimulation.py --prefix tight --motifName1 GATA_disc1 --motifName2 GATA_disc1 --seqLength 200 --numSeq 1000 --minSpacing 0 --meanSpacing 1 --maxSpacing 5
 
 #cleanup the _info files
 rm *_info.txt
@@ -15,11 +15,11 @@ gzip -f *.fa
 
 ###
 #make the labels file without a title, to be shuffled
-zcat variableSpacingGrammarSimulation_*.simdata.gz | perl -lane 'if ($. > 1) {print "$F[0]\t1"}' > labels_without_title.txt
-zcat motifGrammarSimulation_*.simdata.gz | perl -lane 'if ($. > 1) {print "$F[0]\t0"}' >> labels_without_title.txt
+zcat variableSpacingGrammarSimulation_prefix-broad*.simdata.gz | perl -lane 'if ($. > 1) {print "$F[0]\t1"}' > labels_without_title.txt
+zcat variableSpacingGrammarSimulation_prefix-tight*.simdata.gz | perl -lane 'if ($. > 1) {print "$F[0]\t0"}' >> labels_without_title.txt
 
 #concatenate the fasta files to be one per line
-zcat variableSpacingGrammarSimulation_*.fa.gz motifGrammarSimulation_twoMotifs_*.fa.gz | perl -lane 'BEGIN {$title=undef} {if ($.%2==1) {$title=$_} else {print $title."|".$_}}' | gzip -c > concatenated_single_line_inputs.gz
+zcat variableSpacingGrammarSimulation_*.fa.gz | perl -lane 'BEGIN {$title=undef} {if ($.%2==1) {$title=$_} else {print $title."|".$_}}' | gzip -c > concatenated_single_line_inputs.gz
 
 #shuffle the lines
 shuffle_corresponding_lines labels_without_title.txt concatenated_single_line_inputs.gz
@@ -36,12 +36,11 @@ rm labels_without_title.txt
 rm concatenated_single_line_inputs.gz
 rm shuffled_concatenated_single_line_inputs.gz
 rm variableSpacingGrammarSimulation_*.fa.gz
-rm motifGrammarSimulation_*.fa.gz
 
 mkdir splits
 #make the splits
-zcat labels.txt.gz | perl -lane 'if ($.%10 !=1 and $.%10 != 2) {print $F[0]}' | gzip -c > splits/train.txt.gz
-zcat labels.txt.gz | perl -lane 'if ($.%10==1 and $. > 1) {print $F[0]}' | gzip -c > splits/valid.txt.gz
+zcat labels.txt.gz | perl -lane 'if ($.%10 !=1 and $.%10 != 2) {print $F[0]}' | gzip -c > splits/valid.txt.gz
+zcat labels.txt.gz | perl -lane 'if ($.%10==1 and $. > 1) {print $F[0]}' | gzip -c > splits/train.txt.gz
 zcat labels.txt.gz | perl -lane 'if ($.%10==2) {print $F[0]}' | gzip -c > splits/test.txt.gz
 
 make_hdf5 --yaml_configs make_hdf5_yaml/* --output_dir .
